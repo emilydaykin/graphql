2e4b15ea6aa78c02be1bb42d6d0260af84042471
Emily Daykin <emilydaykin31@gmail.com>
2022-07-20 23:36:18 +0200

    loading spinner component and styling added to category and categoriesPreview, and shop section height updated to place footer at the bottom

--- src/components/categories/CategoriesPreview.jsx
+++ src/components/categories/CategoriesPreview.jsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { CategoriesContext } from '../../contexts/categories.context';
 import CategoryCarousel from './CategoryCarousel';
+import Loader from '../Loader';
 import Footer from '../Footer';
 
 const CategoriesPreview = () => {
@@ -16,14 +17,18 @@ const CategoriesPreview = () => {
   return (
     <>
       <div className='shop'>
-        {Object.keys(categoriesMap).map((title) => (
-          <div key={title} className='shop__section'>
-            <h1 className='shop__category'>
-              {title} gifts <span onClick={() => redirectToCategory(title)}>&rarr;</span>
-            </h1>
-            <CategoryCarousel categoriesMap={categoriesMap} title={title} />
-          </div>
-        ))}
+        {Object.keys(categoriesMap).length === 0 ? (
+          <Loader />
+        ) : (
+          Object.keys(categoriesMap).map((title) => (
+            <div key={title} className='shop__section'>
+              <h1 className='shop__category'>
+                {title} gifts <span onClick={() => redirectToCategory(title)}>&rarr;</span>
+              </h1>
+              <CategoryCarousel categoriesMap={categoriesMap} title={title} />
+            </div>
+          ))
+        )}
       </div>
       <Footer />
     </>
--- src/components/categories/Category.jsx
+++ src/components/categories/Category.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import { CategoriesContext } from '../../contexts/categories.context';
 import ProductCard from '../ProductCard';
+import Loader from '../Loader';
 import Footer from '../Footer';
 
 const Category = () => {
@@ -20,15 +21,15 @@ const Category = () => {
       <div className='shop'>
         <div className='shop__section'>
           <h1 className='shop__category shop__category--individual'>{category} gifts</h1>
-          <div className='product-group product-group--individual'>
-            {products ? (
-              products.map((product) => (
+          {!products ? (
+            <Loader />
+          ) : (
+            <div className='product-group product-group--individual'>
+              {products.map((product) => (
                 <ProductCard product={product} key={product.id} preview={false} />
-              ))
-            ) : (
-              <p>Loading Products...</p>
-            )}
-          </div>
+              ))}
+            </div>
+          )}
         </div>
       </div>
       <Footer />
