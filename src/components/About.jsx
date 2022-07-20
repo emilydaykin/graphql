6965c6036cbb80c8bc18a4e4765d3f992d12b54e
Emily Daykin <emilydaykin31@gmail.com>
2022-07-20 23:34:18 +0200

    updated about page text and icons

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,8 @@ import githubLogo from '../assets/github-logo.png';
 import linkedinLogo from '../assets/linkedin-logo.png';
 import reactLogo from '../assets/react-logo.png';
 import sassLogo from '../assets/sass-logo.png';
-import reduxLogo from '../assets/redux-logo.png';
+import graphqlLogo from '../assets/graphql-logo.png';
+import apolloLogo from '../assets/apollo-logo.png';
 import firebaseLogo from '../assets/firebase-logo.png';
 import Footer from './Footer';
 
@@ -14,10 +15,10 @@ const About = () => {
           <div className='about__description'>
             <h1 className='about__heading'>About</h1>
             <p className='about__text'>
-              Unsure of what to get your friends, family or loved ones for a special occasion? Use
+              Unsure of what to get your friends, family or colleagues for a special occasion? Use
               Gifter to find the right present for Christmas, their birthday, wedding day, your
-              anniversary or simply as a thank you token. Browse inspiring gifts for him, for her or
-              for them, any time of year!
+              anniversary or simply as a thank you token. Browse inspiring gifts for your loved
+              ones, any time of year!
             </p>
           </div>
           <div className='about__contact'>
@@ -44,30 +45,21 @@ const About = () => {
               <a href='https://sass-lang.com/'>
                 <img className='about__icon' src={sassLogo} alt='Sass' />
               </a>
-              <a href='https://redux.js.org/'>
-                <img className='about__icon' src={reduxLogo} alt='Redux' />
-              </a>
               {/* <a href='#'>
-            <img className='about__icon' src={reduxSagaLogo} alt='Redux Saga' />
-          </a> */}
-              {/* <a href='#'>
-            <img className='about__icon' src={jestLogo} alt='Jest' />
-          </a> */}
+                <img className='about__icon' src={jestLogo} alt='Jest' />
+              </a> */}
               <a href='https://firebase.google.com/'>
                 <img className='about__icon' src={firebaseLogo} alt='Firebase' />
               </a>
               {/* <a href='#'>
-            <img className='about__icon' src={stripeLogo} alt='Stripe' />
-          </a>
-          <a href='#'>
-            <img className='about__icon' src={apolloLogo} alt='Apollo' />
-          </a>
-          <a href='#'>
-            <img className='about__icon' src={graphQLLogo} alt='GraphQL' />
-          </a>
-          <a href='#'>
-            <img className='about__icon' src={stripeLogo} alt='Stripe' />
-          </a> */}
+                <img className='about__icon' src={stripeLogo} alt='Stripe' />
+              </a>*/}
+              <a href='https://graphql.org/'>
+                <img className='about__icon' src={graphqlLogo} alt='GraphQL' />
+              </a>
+              <a href='https://www.apollographql.com/'>
+                <img className='about__icon' src={apolloLogo} alt='Apollo' />
+              </a>
             </div>
           </div>
         </div>
