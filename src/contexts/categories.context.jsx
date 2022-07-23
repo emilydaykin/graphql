afb23e2fa393d30c8e126a18a958999ff96119ba
Emily Daykin <emilydaykin31@gmail.com>
2022-07-23 12:57:37 +0200

    updated category context to read data via GraphQL instead of firestore

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,32 +1,63 @@
 import { createContext, useState, useEffect } from 'react';
+import { gql, useQuery } from '@apollo/client';
 
 // Uncomment to add items to DB
 // import allProducts from '../data/shop-data.js';
 // import { addCollectionAndDocuments } from '../firebase/firebase.utils';
 
-import { getCategoriesAndDocuments } from '../firebase/firebase.utils';
+// import { getCategoriesAndDocuments } from '../firebase/firebase.utils';
 
 export const CategoriesContext = createContext({
-  // products: []
   categoriesMap: {}
 });
 
+const COLLECTIONS = gql`
+  query {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
+
 export const CategoriesProvider = ({ children }) => {
-  // const [products, setProducts] = useState(allProducts);
+  const { loading, error, data } = useQuery(COLLECTIONS);
   const [categoriesMap, setCategoriesMap] = useState({});
 
+  // console.log('loading:', loading);
+  // console.log('data:', data);
+
+  // --- NO LONGER USING THIS NOW (fetching from firebase) SINCE WE'RE USING GRAPHQL --- //
+  // useEffect(() => {
+  //   // Uncomment to add items to DB
+  //   // addCollectionAndDocuments('categories', allProducts); // 'categories' is the name i want for the collection
+
+  //   const getCategoriesMap = async () => {
+  //     const categoryMap = await getCategoriesAndDocuments();
+  //     // console.log('categoryMap', categoryMap);
+  //     setCategoriesMap(categoryMap);
+  //   };
+  //   getCategoriesMap();
+  // }, []);
+
   useEffect(() => {
-    // Uncomment to add items to DB
-    // addCollectionAndDocuments('categories', allProducts); // 'categories' is the name i want for the collection
+    if (data) {
+      const { collections } = data;
+      const collectionsMap = collections.reduce((acc, collection) => {
+        const { title, items } = collection;
+        acc[title.toLowerCase()] = items;
+        return acc;
+      }, {});
+      setCategoriesMap(collectionsMap);
+    }
+  }, [data]);
 
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      // console.log('categoryMap', categoryMap);
-      setCategoriesMap(categoryMap);
-    };
-    getCategoriesMap();
-  }, []);
-
-  const value = { categoriesMap };
+  const value = { categoriesMap, loading };
   return <CategoriesContext.Provider value={value}>{children}</CategoriesContext.Provider>;
 };
